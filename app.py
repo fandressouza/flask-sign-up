6d0b8cb5b2b28d69b1d6d522581d34ef52a2484e
fandressouza <contact@programming-mentor.com>
2020-11-15 16:55:51 +0000

    setting up mongo connection, creating .env file and adding .gitignore, pulling it using os from app.py

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect
+from functools import wraps
+import pymongo
+import os
 
 app = Flask(__name__)
+# app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
+
+# Get MONGO_URI from .env
+MONGO_URI = os.environ.get('MONGO_URI')
+
+# Database
+client = pymongo.MongoClient(MONGO_URI)
+db = client.user_login_system
+
+# Decorators
+def login_required(f):
+  @wraps(f)
+  def wrap(*args, **kwargs):
+    if 'logged_in' in session:
+      return f(*args, **kwargs)
+    else:
+      return redirect('/')
+  
+  return wrap
 
 # Routes
 from user import routes
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+  return render_template('home.html')
+
+@app.route('/dashboard/')
+@login_required
+def dashboard():
+  return render_template('dashboard.html')
